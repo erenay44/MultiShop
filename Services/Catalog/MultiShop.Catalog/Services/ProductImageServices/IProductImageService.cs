ba5319318941b594ed55ddf9e149d515e7720e01
erenay44 <142754370+erenay44@users.noreply.github.com>
2026-06-03 20:01:10 +0300

    ProductImagesID changed for ProductImageId, ProductImage services added

---
+++ Services/Catalog/MultiShop.Catalog/Services/ProductImageServices/IProductImageService.cs
@@ -0,0 +1,14 @@
+﻿using MultiShop.Catalog.Dtos.ProductImageDtos;
+
+namespace MultiShop.Catalog.Services.ProductImageServices
+{
+    public interface IProductImageService
+    {
+        Task<List<ResultProductImageDto>> GetAllProductImagesAsync();
+        Task CreateProductImageAsync(CreateProductImageDto createProductImageDto);
+        Task UpdateProductImageAsync(UpdateProductImageDto updateProductImageDto);
+        Task DeleteProductImageAsync(string id);
+        Task<GetByIdProductImageDto> GetByIdProductImageDto(string id);
+
+    }
+}
