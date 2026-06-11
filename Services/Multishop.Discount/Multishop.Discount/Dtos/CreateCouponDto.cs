e1abd542c04efecf65f897572d6a2550148f6185
erenay44 <142754370+erenay44@users.noreply.github.com>
2026-06-11 18:36:03 +0300

    Dto's entites added

--- Services/Multishop.Discount/Multishop.Discount/Dtos/CreateCouponDto.cs
+++ Services/Multishop.Discount/Multishop.Discount/Dtos/CreateCouponDto.cs
@@ -2,5 +2,9 @@
 {
     public class CreateCouponDto
     {
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
     }
 }
--- Services/Multishop.Discount/Multishop.Discount/Dtos/ResultCouponDto.cs
+++ Services/Multishop.Discount/Multishop.Discount/Dtos/ResultCouponDto.cs
@@ -2,5 +2,10 @@
 {
     public class ResultCouponDto
     {
+        public int CouponId { get; set; }
+        public string Code { get; set; }
+        public int Rate { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime ValidDate { get; set; }
     }
 }
