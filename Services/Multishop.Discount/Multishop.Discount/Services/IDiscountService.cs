738100420b80922c1fbe44ffaba42b27c6893727
erenay44 <142754370+erenay44@users.noreply.github.com>
2026-06-11 18:56:06 +0300

    createCouponAsync için ilk başta verileri sütunlara eklemek için dapper tabanlı bağlantıları kuruldu. En sonunda doğru yere iletilmesi içinde veritabanı bağlantısı kuruldu

---
+++ Services/Multishop.Discount/Multishop.Discount/Services/IDiscountService.cs
@@ -0,0 +1,13 @@
+﻿using Multishop.Discount.Dtos;
+
+namespace Multishop.Discount.Services
+{
+    public interface IDiscountService
+    {
+        Task<List<ResultCouponDto>> GetAllCouponAsync();
+        Task CreateCouponAsync(CreateCouponDto createCouponDto);
+        Task UpdateCouponAsync(UpdateCouponDto updateCouponDto);
+        Task DeleteCouponAsync(int id);
+        Task<GetByIdCouponDto> GetByCouponIdAsync(int id);
+    }
+}
