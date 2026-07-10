245783dfbe5cc083ad83ff0b6d8c340e30682093
erenay44 <erenayceyran@gmail.com>
2026-07-10 10:33:02 +0300

    Id'ye göre item adres getirme işlemi handlerı yazıldı ve bütün hepsini getirme

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Handlers/AddresHandlers/GetAddressByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using MultiShop.Order.Application.Features.CQRS.Queries.AddressQueries;
+using MultiShop.Order.Application.Features.CQRS.Results.AddressResults;
+using MultiShop.Order.Application.Interfaces;
+using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Handlers.AddresHandlers
+{
+    public class GetAddressByIdQueryHandler
+    {
+        private readonly IRepository<Address> _repository;
+
+        public GetAddressByIdQueryHandler(IRepository<Address> repository)
+        {
+            _repository = repository;
+        }
+        public async Task<GetByAddressIdQueryResult> Handle(GetAddressByIdQuery query)
+        {
+            var values = await _repository.GetByIdAsync(query.Id);
+            return new GetByAddressIdQueryResult
+            {
+                AddressId = values.AddressId,
+                City = values.City,
+                District = values.District,
+                Detail =values.Detail,
+                UserId = values.UserId
+            };
+        }
+    }
+}
