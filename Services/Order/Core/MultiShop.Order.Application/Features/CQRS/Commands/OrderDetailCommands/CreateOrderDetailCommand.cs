7a59b33217bb20e11cbfc7423b4f4e152779e764
erenay44 <erenayceyran@gmail.com>
2026-07-21 16:45:05 +0300

    OrderDetail Commands added

---
+++ Services/Order/Core/MultiShop.Order.Application/Features/CQRS/Commands/OrderDetailCommands/CreateOrderDetailCommand.cs
@@ -0,0 +1,19 @@
+﻿using MultiShop.Order.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiShop.Order.Application.Features.CQRS.Commands.OrderDetailCommands
+{
+    public class CreateOrderDetailCommand
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public int ProductAmount { get; set; }
+        public decimal ProductTotalPrice { get; set; }
+        public string OrderingId { get; set; }
+    }
+}
